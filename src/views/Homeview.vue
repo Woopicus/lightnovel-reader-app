f8c52f15c4aec2e6595492e8b4b62520d7421cb1
Woopicus <bart_drongelen@hotmail.com>
2025-05-06 09:30:39 +0200

    Ik wil als developer repository met vuetify geconfigureerd

--- src/views/Homeview.vue
+++ src/views/Homeview.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <v-card text="Hello world"></v-card>
+    <TheWelcome />
   </main>
 </template>
